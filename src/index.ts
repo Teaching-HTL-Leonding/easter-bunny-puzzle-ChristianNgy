3632113aebd13167d6ee930352192825900615ee
ChristianNgy <c.nagy@students.htl-leonding.ac.at>
2025-04-24 10:26:53 +0200

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,51 @@
 import p5 from 'p5';
+
 let bunnyImage: p5.Image;
+const TILES = 3;
+
+const randomTileX: number[][] = [];
+const randomTileY: number[][] = [];
 
 const p = new p5((sketch) => {
   sketch.setup = setup;
-  sketch.preload = preload;
   sketch.draw = draw;
+  sketch.preload = preload;
 });
 
 function preload() {
   bunnyImage = p.loadImage('https://cddataexchange.blob.core.windows.net/data-exchange/bunny.png');
 }
+
 function setup() {
   p.createCanvas(500, 500);
   p.background('red');
-}
 
+  const tileSize = 500 / TILES;
+  for (let x = 0; x < TILES; x++) {
+    randomTileX[x] = [];
+    randomTileY[x] = [];
+    for (let y = 0; y < TILES; y++) {
+      randomTileX[x][y] = Math.floor(p.random(0, TILES)) * tileSize;
+      randomTileY[x][y] = Math.floor(p.random(0, TILES)) * tileSize;
+    }
+  }
+}
 function draw() {
-  p.background('white');
-  p.stroke('white');
   p.noFill();
+  p.stroke('white');
   p.strokeWeight(2);
 
-  for (let y = 0; y <= 500; y += 100) {
-    for (let x = 0; x <= 500; x += 100) {
-      p.image(bunnyImage, 0 + x, 0 + y, 100, 100, 0 + x, 0 + y, 100, 100);
-        p.rect(0 + x, 0 + y, 100, 100);
+  for (let x = 0; x < TILES; x++) {
+    for (let y = 0; y < TILES; y++) {
+        if(x !== TILES - 1 || y !== TILES - 1) {
+       
+            const imageX = randomTileX[x][y];
+            const imageY = randomTileY[x][y];
+            const tileSize = 500 / TILES;
+            
+            p.image(bunnyImage, tileSize * x, tileSize * y, tileSize, tileSize, imageX, imageY, tileSize, tileSize);
+            p.rect(imageX, imageY, tileSize, tileSize);
+        }
     }
   }
 }
